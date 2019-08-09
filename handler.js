4ac0e1de298dc5f3790546840a74438603dc4014
Steve <steve@lemoncloud.io>
2019-08-09 23:17:53 +0900

    update core project files

--- handler.js
+++ handler.js
@@ -1,28 +1,36 @@
-/* eslint-disable import/no-dynamic-require */
+/**
+ * `handler.js`
+ * - handler to be exported for serverless.
+ * - ONLY FOR Lambda Deployment.
+ *
+ *
+ * @author  Steve Jung <steve@lemoncloud.io>
+ * @date    2019-07-31 cleanup for ts support.
+ * @date    2019-08-09 optimized with `lemon-core#1.0.1`
+ *
+ * @copyright (C) lemoncloud.io 2019 - All Rights Reserved.
+ */
 /** ********************************************************************************************************************
  *  Serverless API Handlers
  *  - optimized to export handler functions for serverless.
  *  - mainly export API handler which is includes in main loader.
- ** ****************************************************************************************************************** */
+ ** *******************************************************************************************************************/
+//NOTE - 다음이 있어야, Error 발생시 ts파일에서 제대로된 스택 위치를 알려줌!!!.
+require('source-map-support').install();
+
 // COMMON ENVIRONMENT LOADER
 const $env = process.env || {}; // NOTE! - serverless may initialize environment with opt.
 
 //! SETUP TIMEZONE @2019/03/14
-process.env.TZ = 'Asia/Seoul';
+$env.TZ = 'Asia/Seoul';
 
 //! TARGET SOURCE FOLDER.
 const SRC = $env.SRC || './dist/';
 
-//! PREPARE EXPORT SCOPE.
-const $scope = { name: 'lemon-hello-api' };
-
 //! load configuration.
-const $lemon = require(`${SRC}index`)($scope, $env);
+const engine = require(`${SRC}index`).engine;
+const $engine = engine();
+if (!$engine) throw new Error('$engine is required! check environ.');
 
-//! Load Additional Handlers
-const SNS = require(`${SRC}SNS`)($lemon);
-const SQS = require(`${SRC}SQS`)($lemon);
-const WSS = require(`${SRC}WSS`)($lemon);
-
-//! export serverless handlers.
-module.exports = Object.assign($scope, { SNS, WSS, SQS });
+//! export as serverless handlers.
+module.exports = $engine;
