f18b7b8be473c4b23a60b5c6cfab066e552456ca
Steve <steve@lemoncloud.io>
2019-08-10 01:03:47 +0900

    fix: test hello-api

--- test/test.hello-api.ts
+++ test/test.hello-api.ts
@@ -1,6 +1,6 @@
 /**
- * `test.hello-api.js`
- * - test runnder for api
+ * `test.hello-api.ts`
+ * - test runnder of hello-api
  *
  *
  * @author Steve <steve@lemoncloud.io>
@@ -10,7 +10,7 @@
  */
 import request from 'supertest';
 import { app } from '../src/express';
-const $lemon = express['$lemon'];
+import { $kms, $s3s, $sns } from '../src/engine';
 
 // Test Hello
 describe('Test Hello API', () => {
@@ -18,7 +18,7 @@ describe('Test Hello API', () => {
     test('It should response the GET method', done => {
         request(app)
             .get('/')
-            .then(response => {
+            .then((response: any) => {
                 expect(response.statusCode).toBe(200);
                 done();
             });
@@ -31,12 +31,12 @@ describe('Test Hello API', () => {
     });
 
     test('It should get the proper instances', () => {
-        expect($lemon.kms.hello().hello).toBe('kms-service');
-        expect($lemon.sns.hello().hello).toBe('sns-service');
-        expect($lemon.s3s.hello().hello).toBe('s3s-service');
+        expect($kms.hello().hello).toBe('kms-service');
+        expect($sns.hello().hello).toBe('sns-service');
+        expect($s3s.hello().hello).toBe('s3s-service');
     });
 
     test('It should get the unique id', () => {
-        expect($lemon.s3s.nextId().length).toBe('583b839c-aa9d-4ea1-a2d7-2e374ee1566a'.length);
+        expect($s3s.nextId().length).toBe('583b839c-aa9d-4ea1-a2d7-2e374ee1566a'.length);
     });
 });
